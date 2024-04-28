3f2710a07cccbe27ad4a3d001ecf7d816559e358
Luka Grabnar <grabnar.luka@gmail.com>
2024-04-28 23:17:06 +0200

    api auth

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,7 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function () {
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1', 'middleware' => 'auth:sanctum'], function () {
     Route::apiResource('customers', CustomerController::class);
     Route::apiResource('invoices', InvoiceController::class);
     Route::post('invoices/bulk', ['uses' => 'InvoiceController@bulkStore']);
